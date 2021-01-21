6d3f06cec429aa5b7eae01212ad49bee7915467e
priyaps1989 <62037911+priyaps1989@users.noreply.github.com>
2021-01-22 08:08:31 +1300

    Add files via upload

---
+++ Knight Tour/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Knight_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChessBoard.CreateChessBoard();
+            ChessBoard.DisplayChessBoard();
+            // KnightMoves.KnightTour();
+            Knight_Tour__EFficient_Approach.KnightTour();
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
